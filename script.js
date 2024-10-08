30b0db320aac07c8e40734b1eef6ce428553ce88
chirag-dhingra <124273981+chirag-dhingra@users.noreply.github.com>
2024-10-08 16:49:11 +0200

    const and let, let can be asigned different types of values

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-console.log("Hello World!")
+const btn = document.querySelector(".btn-open");
+const form = document.querySelector(".fact-form");
+btn.addEventListener('click', function() {
+    if(form.classList.contains("hidden")){
+        form.classList.remove("hidden")
+        btn.textContent = "CLOSE";
+    }
+    else{
+        form.classList.add("hidden")
+        btn.textContent = "SHARE A FACT";
+    }
+})
+
+let votesInteresting = 24 //Changable variable because of let
+let votesMindBlowing = 9 //Changable variable because of let
+
+const fact = "Lisbon is a capital of Portugal" //Unchnangable variable because of let
